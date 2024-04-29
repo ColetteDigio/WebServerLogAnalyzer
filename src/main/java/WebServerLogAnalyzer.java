562be4a7a807965afcd7c7db9c6ff9b0fd4bc11b
coletteliu <colette.liu@foxtel.com.au>
2024-04-29 14:00:04 +1000

    assessment - add menthod to read log file and extract into list of log

--- src/main/java/WebServerLogAnalyzer.java
+++ src/main/java/WebServerLogAnalyzer.java
@@ -1,9 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WebServerLogAnalyzer {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        List<String> logs = new ArrayList<>();
 
         // Read from the log file
+        String logFilePath = "/Users/coletteliu/Documents/assessment/WebServerLogAnalyzer/src/main/resources/programming-task-example-data.log";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(logFilePath));
+
+            String line;
+            while((line = br.readLine()) != null) {
+                logs.add(line);
+                System.out.println(line);
+            }
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
 
 
     }
