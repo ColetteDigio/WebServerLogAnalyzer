b03e5caf93a2a30b7b368373454e22e80a60ee26
coletto817 <colette.liu@gmail.com>
2024-04-30 15:39:27 +1000

    assessment - add url path only in the implementation for urlCount

--- src/main/java/WebServerLogAnalyzer.java
+++ src/main/java/WebServerLogAnalyzer.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -26,6 +26,7 @@ public class WebServerLogAnalyzer {
                 logs.add(line);
             }
         } catch (IOException ex) {
+            // TODO handle the error gracefully
             ex.printStackTrace();
         }
 
@@ -43,24 +44,48 @@ public class WebServerLogAnalyzer {
             String ip = log.split(" - ")[0];
             ipCount.put(ip, ipCount.getOrDefault(ip, 0) + 1);
 
-            String url = log.split("\"")[1].split(" ")[1];
-            urlCount.put(url, urlCount.getOrDefault(url, 0) + 1);
-            // TODO the URLs structure are not consistent, check and observe
+            structurePathData(log, urlCount);
         }
 
-        // print out IPs to observe the number of times IP is visited
-        System.out.println("\n*** SUMMARIES OF IPs Count   ***");
-        ipCount.forEach((key, value) ->  System.out.println(key + " = " + value));
-
-        // print out URLs to observe the structure
-        System.out.println("\n*** SUMMARIES OF URLs Count   ***");
-        urlCount.forEach((key, value) ->  System.out.println(key + " = " + value));
+//        // print out IPs to observe the number of times IP is visited
+//        System.out.println("\n***   SUMMARIES OF IPs Count   ***");
+//        ipCount.forEach((key, value) ->  System.out.println(key + " = " + value));
+//
+//        // print out URLs to observe the structure
+//        System.out.println("\n***   SUMMARIES OF URLs Count   ***");
+//        urlCount.forEach((key, value) ->  System.out.println(key + " = " + value));
 
         // Get Top 3 after sorting out the data
         List<String> top3ActiveIps = getTop3(ipCount, 3);
         List<String> top3VisitedURLs = getTop3(urlCount, 3);
 
         return new LogDetails(ipCount.size(),top3VisitedURLs, top3ActiveIps);
+    }
+
+    // clean up the URLs structure, ignore domain and looking at the url path only
+    private static void structurePathData(String log, Map<String, Integer> urlCount) {
+        String fullURL = log.substring(log.indexOf("GET ") + 4, log.indexOf(" HTTP"));
+
+        String path = "";
+
+        if (fullURL.startsWith("http")) {
+            try {
+                URI uri = new URI(fullURL);
+                path = uri.getPath();
+            } catch (URISyntaxException e) {
+                // TODO handle the error gracefully
+                e.printStackTrace();
+            }
+        } else {
+            path = fullURL;
+        }
+
+        if (urlCount.containsKey(path)) {
+            urlCount.put(path, urlCount.get(path) + 1); // increment the instance
+        } else if (!path.isEmpty()) {
+            // ignores all the spaces at the bottom of the log
+            urlCount.put(path, 1); // create an instance
+        }
     }
 
     // a method that will look at each pair of <Ips and number> && <Urls and number> and sorting it out in order
